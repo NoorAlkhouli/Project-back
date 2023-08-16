dbfe368ee95cc3997b84f4ceed515cd2e7b4ccc3
nooralkh <142157291+NoorAlkhouli@users.noreply.github.com>
2023-08-17 01:10:25 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,24 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+     public function boot(UrlGenerator $url)
+    {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+    }
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+        
     }
 
     /**
